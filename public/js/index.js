846a75269d5ee8a1c6d0b39c68d0c054401aaacc
Duy Hoang Nguyen <duyhoangnguyen2000@gmail.com>
2023-08-06 14:42:42 -0700

    still a bug with redo button

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,8 @@
 import { wait_time_ms, test_type, TEXT_TRANSFORM_PATH, AUTOCOMPLETE_PATH, speed_fast, speed_normal, speed_slow } from "./constants.js";
 let message = document.getElementById('messagebox');
-let tone = document.getElementById('tonebox');
+let toneBox = document.getElementById('tonebox');
+let toneInput = document.getElementById('tone_input');
+
 // const istyping = document.getElementById('istyping')
 let timeout = setTimeout(function () { }, 0)
 
@@ -14,38 +16,51 @@ const autocompleteView = document.getElementById('autocomplete-view')
 const transformView = document.getElementById('transform-view')
 const autocompleteViewButton = document.getElementById('autocomplete-view-btn')
 const transformViewButton = document.getElementById('transform-view-btn')
+const redoButton = document.getElementById('redo-button')
 
 async function submitTone() {
   //TODO: Call API and passed the value and message as body. (/text-transform)
   let data = new FormData()
   data.append("text", message.textContent)
-  data.append("tone", tone.value)
+  if(toneInput.value !== '') {
+    data.append("tone", toneInput.value)
+  } else {
+    data.append("tone", toneBox.value)
+  }
   let transformResponse = await fetch(TEXT_TRANSFORM_PATH, {method: "POST", body: data})
   let transformText = await transformResponse.text()
   console.log(transformText)
-  const desiredOutput = tone.value + ": " + transformText
-
-  return output.innerHTML = desiredOutput
+  const desiredOutput = transformText
+  outputTypewriter(desiredOutput)
 }
 
 function reset() {
-  output.innerHTML = " "
+  message.textContent = '';
+  message.innerHTML += `<span id="tmp_text"></span>`;
+  outputTimeoutArray.forEach((e) => {
+    clearTimeout(e)
+  })
+  output.textContent = '';
+  toneInput.value = '';
+  toneBox.value = 'formal';
 }
 
 // Code to switch views from https://stackoverflow.com/questions/25981198/how-to-hide-one-div-and-show-another-div-using-button-onclick
 function switchView() {
   let autocompleteView = document.getElementById('autocomplete-view')
   let transformView = document.getElementById('transform-view')
-
   if (transformView !== undefined) {
-    if (autocompleteView.style.display == 'none') {
+    if (autocompleteView.style.display === 'none' || autocompleteView.style.display === '') {
       transformView.style.display = 'none'
       autocompleteView.style.display = 'block'
+      message.addEventListener('keydown', autocomplete);
     } else {
       transformView.style.display = 'block'
       autocompleteView.style.display = 'none'
+      message.removeEventListener('keydown', autocomplete);
     }
   }
+  reset();
 }
 
 
@@ -70,6 +85,7 @@ function placeCaretAtEnd(el) {
   }
 }
 
+let autocompleteTimeoutArray = []
 /**
  * Take a string append it character by character to the tmp textbox. Put the
  * cursor at the very end of the tmp textbox afterward.
@@ -90,12 +106,12 @@ function typeWriter(txt) {
     speed = speed_slow;
   };
   for (let i = 0; i < txt.length; i++ ) {
-    setTimeout( function () {
+    autocompleteTimeoutArray[i] = setTimeout( function () {
       let tmp_text = document.getElementById('tmp_text');
       tmp_text.textContent += txt.charAt(i);
     },speed * i);
   }
-  setTimeout(() => {
+  autocompleteTimeoutArray[txt.length] = setTimeout(() => {
     message.contentEditable = true;
     message.focus();
     placeCaretAtEnd(message);
@@ -106,48 +122,6 @@ function typeWriter(txt) {
 
 let autocompleteText = '';
 
-message.addEventListener('keydown', (e) => {
-  console.log("Entering autocomplete")
-  if (autocompleteView === undefined || autocompleteView === 'none') return
-  console.log()
-  if (e.key.length === 1) {
-    clearTimeout(timeout);
-    console.log('User is typing')
-    timeout = setTimeout(async function() {
-      let tmp_text = document.getElementById('tmp_text');
-      console.log(tmp_text.textContent)
-      if (autocompleteText.length === 0 && tmp_text.textContent.length === 0) {
-        //TODO: Call API and passed the vlaue and message as body.
-        let data = new FormData()
-        data.append("text", message.textContent)
-        data.append("tone", tone.value)
-
-        let autocompleteResponse = await fetch(AUTOCOMPLETE_PATH, {method: "POST", body: data})
-        autocompleteText = await autocompleteResponse.text()
-        console.log(autocompleteText)
-        //TODO: typeWriter will be call once backend return a content.
-        typeWriter(autocompleteText);
-      }
-      console.log('User is not typing');
-      console.log(autocompleteText.length);
-      }, wait_time_ms)
-  } else if (e.key === 'Tab') {
-    e.preventDefault();
-    e.stopPropagation();
-    let tmp_text = document.getElementById('tmp_text');
-    let tmp = tmp_text.textContent;
-    tmp_text.textContent = '';
-    message.textContent += tmp;
-    message.innerHTML += `<span id="tmp_text"></span>`;
-  } else if (e.key === "Backspace" || e.key === "Delete") {
-    let tmp_text = document.getElementById('tmp_text');
-    if(!tmp_text) {
-      message.innerHTML += `<span id="tmp_text"></span>`;
-    }
-  }
-  message.focus();
-  placeCaretAtEnd(message);
-})
 
 submitButton.addEventListener("click", (e) => {
   if (e) {
@@ -172,3 +146,107 @@ transformViewButton.addEventListener("click", (e) => {
     switchView()
   }
 })
+
+function autocomplete(e) {
+  console.log("Entering autocomplete")
+  if (autocompleteView === undefined || autocompleteView === 'none') return
+  console.log()
+  if (e.key.length === 1) {
+    clearTimeout(timeout);
+    console.log('User is typing')
+    timeout = setTimeout(async function() {
+      autocompeteHelper();
+      console.log('User is not typing');
+      console.log(autocompleteText.length);
+      }, wait_time_ms)
+  } else if (e.key === 'Tab') {
+    e.preventDefault();
+    e.stopPropagation();
+    let tmp_text = document.getElementById('tmp_text');
+    let tmp = tmp_text.textContent;
+    tmp_text.textContent = '';
+    message.textContent += tmp;
+    message.innerHTML += `<span id="tmp_text"></span>`;
+  } else if (e.key === "Backspace" || e.key === "Delete") {
+    let tmp_text = document.getElementById('tmp_text');
+    console.log(tmp_text === null)
+    if(tmp_text === null) {
+      e.preventDefault();
+      message.innerHTML += `<span id="tmp_text"></span>`;
+    } else if (tmp_text.textContent !== '') {
+      clearTimeout(timeout);
+      e.preventDefault();
+      e.stopPropagation();
+      tmp_text.textContent = '';
+      timeout = setTimeout(async function() {
+        if (message.textContent !== '') {
+          autocompeteHelper();
+        }
+        console.log('User is not typing');
+        console.log(autocompleteText.length);
+        }, wait_time_ms)
+    } else if (tmp_text.textContent === '' && message.textContent === '') {
+      e.preventDefault();
+    } 
+  } else {
+    clearTimeout(timeout)
+  }
+  message.focus();
+  placeCaretAtEnd(message);
+}
+
+redoButton.addEventListener('click', async (e) => {
+  let tmp_text = document.getElementById('tmp_text');
+  autocompleteTimeoutArray.forEach((e) => {
+    clearTimeout(e)
+  })
+  message.contentEditable = true;
+  message.focus();
+  placeCaretAtEnd(message);
+  if (tmp_text.textContent !== '') {
+    tmp_text.textContent = '';
+  }
+  autocompeteHelper();
+})
+
+async function autocompeteHelper() {
+  let tmp_text = document.getElementById('tmp_text');
+  if (autocompleteText.length === 0 && tmp_text.textContent.length === 0) {
+    //TODO: Call API and passed the vlaue and message as body.
+    let data = new FormData()
+    data.append("text", message.textContent)
+    if(toneInput.value !== '') {
+      data.append("tone", toneInput.value)
+    } else {
+      data.append("tone", toneBox.value)
+    }
+      let autocompleteResponse = await fetch(AUTOCOMPLETE_PATH, {method: "POST", body: data})
+    autocompleteText = await autocompleteResponse.text()
+    console.log(autocompleteText)
+    //TODO: typeWriter will be call once backend return a content.
+    typeWriter(autocompleteText);
+  }
+}
+let outputTimeoutArray = [];
+function outputTypewriter(txt) {
+  if (!resetButton.disabled) {
+    resetButton.disabled = true;
+    let speed = 0;
+    output.textContent = '';
+    if (txt.length > 150) {
+      speed = speed_fast;
+    } else if (txt.length > 75) {
+      speed = speed_normal;
+    } else {
+      speed = speed_slow;
+    };
+    for (let i = 0; i < txt.length; i++ ) {
+      outputTimeoutArray[i] = setTimeout( function () {
+        output.textContent += txt.charAt(i);
+      },speed * i);
+    }
+    setTimeout(() => {
+      resetButton.disabled = false;
+    }, speed * txt.length);
+  }
+}
